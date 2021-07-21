500e1cb5bf315f7c0f58c5f22c8319cf2a9625b0
brendisurfs <brendan.prednis@gmail.com>
2021-07-21 14:11:13 -0700

    first set of questions on Go-cli project.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -17,6 +18,15 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
+
+	// if the os args dont match, print custom error.
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)
+	}
+
+	//  assign to our globally defined originUnit
+	originUnit = strings.ToUpper(os.Args[1])
+	
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
